2c6c4d26a13bf0426db2fa83eaa2d089673258f8
Eric Ly <102763906+eeeerrrriiiicccc@users.noreply.github.com>
2022-06-14 09:14:08 +1200

    Create ormistoncafe.py

---
+++ ormistoncafe.py
@@ -0,0 +1,19 @@
+class OrmistonCafepg2:
+    def __init__(self, parent):
+        self.parent = parent
+
+    home = LabelFrame(currency_frame, text="Order Now")
+    home.pack(pady=20)
+
+    home_button = Button(home, font=("Helvetica", 24))
+    home_button.pack(pady=10, padx=10)
+
+class OrmistonCafepg3:
+    def __init__(self, parent):
+        self.parent = parent
+
+    home = LabelFrame(currency_frame, text="Order Now")
+    home.pack(pady=20)
+
+    home_button = Button(home, font=("Helvetica", 24))
+    home_button.pack(pady=10, padx=10)
