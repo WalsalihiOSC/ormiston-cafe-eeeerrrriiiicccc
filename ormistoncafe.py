5017b7d922e6f82a89ed7964ec7131c24b11ee3c
Eric Ly <102763906+eeeerrrriiiicccc@users.noreply.github.com>
2022-06-14 09:14:46 +1200

    Update ormistoncafe.py

--- ormistoncafe.py
+++ ormistoncafe.py
@@ -1,3 +1,32 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+root = Tk()
+root.title("Ormiston Cafe")
+root.geometry("500x500")
+
+# Create tabs
+notebook = ttk.Notebook(root)
+notebook.pack(pady=5)
+
+# Create two frames
+currency_frame = Frame(notebook, width=480, height=480)
+conversion_frame = Frame(notebook, width=480, height=480)
+
+currency_frame.pack(fill="both", expand=1)
+conversion_frame.pack(fill="both", expand=1)
+
+class OrmistonCafepg1:
+    def __init__(self, parent):
+        self.parent = parent
+
+    home = LabelFrame(currency_frame, text="Order Now")
+    home.pack(pady=20)
+    
+    home_button = Button(home, font=("Helvetica", 24))
+    home_button.pack(pady=10, padx=10)
+
 class OrmistonCafepg2:
     def __init__(self, parent):
         self.parent = parent
@@ -17,3 +46,5 @@ class OrmistonCafepg3:
 
     home_button = Button(home, font=("Helvetica", 24))
     home_button.pack(pady=10, padx=10)
+
+root.mainloop()
